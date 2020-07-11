4c9ba7697998ef42b857175dcb03eb909e853ea3
RevanVI <47805713+RevanVI@users.noreply.github.com>
2020-07-11 09:03:19 +0300

    step

--- Assets/Scripts/DayLightController.cs
+++ Assets/Scripts/DayLightController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering.Universal;
 
+using UnityEngine.UI;
+
 public class DayLightController : MonoBehaviour
 {
     public Light2D Light;
@@ -10,6 +12,13 @@ public class DayLightController : MonoBehaviour
     [Tooltip("Day length in seconds")]
     public float DayLength = 86400;
     private float _currentTime;
+
+    //debug 
+    [SerializeField]
+    private Text _dateText;
+    [SerializeField]
+    private Image _colorSample;
+    //
 
     void Start()
     {
@@ -22,5 +31,8 @@ public class DayLightController : MonoBehaviour
         _currentTime = (curTime.Hour * 60 + curTime.Minute) * 60 + curTime.Second;
         float value = _currentTime / DayLength;
         Light.color = LightColor.Evaluate(value);
+
+        _dateText.text = curTime.ToLongTimeString();
+        _colorSample.color = Light.color;
     }
 }
